1f16e0819be279f3097b4af74cf625c76f06f39d
Amrsamy19 <amrsamy622@gmail.com>
2025-11-15 13:41:22 +0200

    featL: installed shadecn and added theme toggle button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
+import { ModeToggle } from "@/components/Toggle";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <ModeToggle/>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
