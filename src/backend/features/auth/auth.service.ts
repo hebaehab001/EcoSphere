106afbc63a2b4a921318fddd447208d44efd142f
marwan <marwanabdalmagied@gmail.com>
2025-11-23 14:29:09 +0200

    refactor: use request dto and fromat the file, we do not use this service most of the time

--- src/backend/features/auth/auth.service.ts
+++ src/backend/features/auth/auth.service.ts
@@ -1,89 +1,106 @@
 import { inject, injectable } from "tsyringe";
 import type { IAuthRepository } from "./auth.repository";
 import {
-  comparePassword,
-  generateToken,
-  hashPassword,
-  OMIT,
+	comparePassword,
+	generateToken,
+	hashPassword,
+	OMIT,
 } from "../../utils/helpers";
 import { Gender, User } from "@/generated/prisma/client";
-import type { LoginDto, RegisterDto } from "./dto/user.dto";
+import type {
+	LoginRequestDTO,
+	RegisterRequestDTO,
+	LoginResponseDTO,
+} from "./dto/user.dto";
+import { IUser } from "../user/user.model";
 
 export interface IAuthService {
-  login(loginDto: LoginDto): Promise<{ token: string; user: Omit<User, "password"> } | null>;
-  register(
-    registerDto: RegisterDto
-  ): Promise<{ token: string; user: Omit<User, "password"> } | null>;
-  logout(): Promise<void>;
+	login(
+		loginDto: LoginRequestDTO
+	): Promise<{ token: string; user: Omit<User, "password"> } | null>;
+	register(
+		registerDto: RegisterRequestDTO
+	): Promise<{ token: string; user: Omit<User, "password"> } | null>;
+	logout(): Promise<void>;
 }
 
 @injectable()
 class AuthService {
-  constructor(
-    @inject("IAuthRepository") private authRepository: IAuthRepository
-  ) { }
+	constructor(
+		@inject("IAuthRepository") private readonly authRepository: IAuthRepository
+	) {}
 
-  public async login({
-    email,
-    password,
-  }: LoginDto): Promise<{ token: string; user: User } | null> {
-    const user = await this.authRepository.findByEmail(email);
-    if (user) {
-      const isMatch = await comparePassword(password, user.password);
-      if (isMatch) {
-        const token = generateToken({ id: user.id, email: user.email, role: user.role });
+	public async login({
+		email,
+		password,
+	}: LoginRequestDTO): Promise<LoginResponseDTO | null> {
+		const user = await this.authRepository.findByEmail(email);
+		if (user) {
+			const isMatch = await comparePassword(password, user.password);
+			if (isMatch) {
+				const token = generateToken({
+					id: user._id!,
+					email: user.email,
+					lastName: user.lastName,
+					role: user.role,
+				});
 
-        const data = OMIT(user, "password");
+				const data = OMIT(user, "password");
 
-        return {
-          token,
-          user: data as User,
-        };
-      }
-    }
-    return null;
-  }
+				return {
+					token,
+					user: data as IUser,
+				};
+			}
+		}
+		return null;
+	}
 
-  public async register({
-    email,
-    name,
-    password,
-    birthDate,
-    address,
-    avatar,
-    gender,
-    phoneNumber,
-  }: RegisterDto): Promise<{ token: string; user: User } | null> {
-    const existingUser = await this.authRepository.findByEmail(email);
-    if (!existingUser) {
-      const hashed = await hashPassword(password);
-      const userGender = Gender[gender as keyof typeof Gender];
-      const user = await this.authRepository.register(
-        email,
-        name,
-        hashed,
-        birthDate,
-        address,
-        avatar,
-        userGender,
-        phoneNumber
-      );
+	public async register({
+		email,
+		name,
+		password,
+		birthDate,
+		address,
+		avatar,
+		gender,
+		phoneNumber,
+	}: RegisterRequestDTO): Promise<{ token: string; user: User } | null> {
+		const existingUser = await this.authRepository.findByEmail(email);
+		if (!existingUser) {
+			const hashed = await hashPassword(password);
+			const userGender = Gender[gender as keyof typeof Gender];
+			const user = await this.authRepository.register(
+				email,
+				name,
+				hashed,
+				birthDate,
+				address,
+				avatar,
+				userGender,
+				phoneNumber
+			);
 
-      const token = generateToken({ id: user.id, email: user.email, role: user.role });
+			const token = generateToken({
+				id: user.id,
+				lastName: "test", // it should be user.lastName
+				email: user.email,
+				role: user.role,
+			});
 
-      return {
-        token,
-        user,
-      };
-    }
-    return null;
-  }
-  public async me(): Promise<void> {
-    return;
-  }
-  public async logout(): Promise<void> {
-    return;
-  }
+			return {
+				token,
+				user,
+			};
+		}
+		return null;
+	}
+	public async me(): Promise<void> {
+		return;
+	}
+	public async logout(): Promise<void> {
+		return;
+	}
 }
 
 export default AuthService;
