13c137d9c4ec542f1e95b9e7508f40fb5d01b426
marwan <marwanabdalmagied@gmail.com>
2025-11-26 21:44:51 +0200

    refactor: add interface to authController and oAuthUserDto

--- src/backend/features/auth/auth.controller.ts
+++ src/backend/features/auth/auth.controller.ts
@@ -2,26 +2,50 @@ import { inject, injectable } from "tsyringe";
 import type {
   LoginRequestDTO,
   LoginResponse,
+  OAuthUserDTO,
   RegisterRequestDTO,
   RegisterResponseDTO,
 } from "./dto/user.dto";
 import type { IRegistrationStrategy } from "./registration/registration.service";
 import { LoginService } from "./login/users.login.service";
 
+export interface IAuthController {
+  loginWithCredentials(loginDto: LoginRequestDTO): Promise<LoginResponse>;
+  LoginWithGoogle(user: OAuthUserDTO, googleId: string): Promise<boolean>;
+  register(registerDto: RegisterRequestDTO): Promise<RegisterResponseDTO>;
+}
+
 @injectable()
-class AuthController {
+class AuthController implements IAuthController {
   constructor(
     @inject("RegistrationService")
     private readonly registrationService: IRegistrationStrategy,
-    @inject("LoginService") private readonly loginService: LoginService
+    @inject("LoginService") private readonly loginService: LoginService,
   ) {}
 
-  async login(loginDto: LoginRequestDTO): Promise<LoginResponse> {
+  async loginWithCredentials(
+    loginDto: LoginRequestDTO,
+  ): Promise<LoginResponse> {
+    if (!loginDto.email || !loginDto.password) {
+      throw new Error("Email and password are required");
+    }
     return await this.loginService.login(loginDto);
   }
 
+  async LoginWithGoogle(user: OAuthUserDTO): Promise<boolean> {
+    const savedUser = await this.loginService.findByEmail(
+      user.email,
+      user.oAuthId
+    );
+
+    if (!savedUser) {
+      await this.registrationService.register(user);
+    }
+    return true;
+  }
+
   async register(
-    registerDto: RegisterRequestDTO
+    registerDto: RegisterRequestDTO,
   ): Promise<RegisterResponseDTO> {
     return await this.registrationService.register(registerDto);
   }
