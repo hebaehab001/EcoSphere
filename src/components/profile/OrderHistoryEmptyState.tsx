ca4848476a9bd4c24882367d3f56385158f44841
Hamada <gunslinger12398@gmail.com>
2025-12-03 13:40:50 +0200

    Localization for Cart Page ( en ) Done

--- src/components/profile/OrderHistoryEmptyState.tsx
+++ src/components/profile/OrderHistoryEmptyState.tsx
@@ -2,8 +2,10 @@
 
 import { ShoppingBag } from "lucide-react";
 import Link from "next/link";
+import { useTranslations } from 'next-intl';
 
 const OrderHistoryEmptyState = () => {
+    const t = useTranslations('Profile.customer');
     return (
         <div className="flex items-center justify-center p-20 bg-primary/10 rounded-xl mt-10 my-20">
             <div className="text-center max-w-md px-6">
@@ -12,15 +14,15 @@ const OrderHistoryEmptyState = () => {
                 </div>
 
                 <h2 className="text-2xl font-semibold text-foreground mb-2">
-                    No orders yet
+                    {t('noOrders')}
                 </h2>
 
                 <p className="text-secondary-foreground mb-6">
-                    Start adding products to your Orders to see them here
+                    {t('noOrdersDescription')}
                 </p>
 
                 <Link href="/store" className="bg-primary text-primary-foreground px-10 py-3 rounded-full transition duration-400 hover:scale-102 text-lg gap-2 hover:outline-2 hover:outline-primary hover:outline-offset-4">
-                    Browse Orders
+                    {t('browseOrders')}
                 </Link>
             </div>
         </div>
