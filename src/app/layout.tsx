2b63c43a2c724ed6ce57c2c7fdbd238b32f4bf07
Heba <hebaehab001@gmail.com>
2025-12-06 01:56:34 +0200

    handle sidebar icon in the small devices

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { Toaster } from "sonner";
 import { SidebarInset } from "@/components/ui/sidebar";
 import { NextIntlClientProvider } from 'next-intl';
 import { getLocale, getMessages } from 'next-intl/server';
+import SidebarTriggerBtn from "@/components/layout/SideBar/SidebarTriggerBtn/SidebarTriggerBtn";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -42,12 +43,9 @@ export default async function RootLayout({
             <Toaster position="top-right" />
             <SideBar />
             <SidebarInset>
-
-              {/* <div className="w-full bg-background text-foreground"> */}
+              <SidebarTriggerBtn/>
               {children}
-              {/* </div> */}
             </SidebarInset>
-
           </Providers>
         </NextIntlClientProvider>
       </body>
---
+++ src/components/layout/SideBar/SidebarTriggerBtn/SidebarTriggerBtn.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { SidebarTrigger, useSidebar } from '@/components/ui/sidebar'
+import React from 'react'
+
+export default function SidebarTriggerBtn() {
+  const { open, isMobile} = useSidebar();
+  console.log(isMobile);
+  
+  return (
+    isMobile ? <SidebarTrigger className={`absolute ${open ? 'hidden' :'absolute'}  top-4 left-4 size-5` }/> : <SidebarTrigger className='hidden'/>
+  )
+}
