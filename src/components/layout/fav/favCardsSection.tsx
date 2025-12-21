6f6f7f9c9e537f0e466c926c7046c38dedca7ef5
marinaeleshaa <marinaeleshah77@gmail.com>
2025-12-21 13:39:45 +0200

    fix: update cursor styles for pagination buttons to improve user interaction fix: no fav items responsiveness

--- src/components/layout/fav/favCardsSection.tsx
+++ src/components/layout/fav/favCardsSection.tsx
@@ -5,10 +5,11 @@ import { useSelector } from "react-redux";
 import FavCard from "./FavCard";
 import { Heart } from "lucide-react";
 import Link from "next/link";
-import { useTranslations } from 'next-intl';
+import { useTranslations } from "next-intl";
+import Pagination from "@/components/ui/Pagination";
 
 const FavCardsSection = () => {
-  const t = useTranslations('Favorites.emptyState');
+  const t = useTranslations("Favorites.emptyState");
   const { view, favProducts } = useSelector((state: RootState) => state.fav);
 
   return (
@@ -21,25 +22,24 @@ const FavCardsSection = () => {
             </div>
 
             <h2 className="text-2xl font-semibold text-foreground mb-2">
-              {t('title')}
+              {t("title")}
             </h2>
 
-            <p className="text-secondary-foreground mb-6">
-              {t('description')}
-            </p>
+            <p className="text-secondary-foreground mb-6">{t("description")}</p>
 
             <Link href="/store" className="myBtnPrimary w-full  mx-auto">
-              {t('browseButton')}
+              {t("browseButton")}
             </Link>
           </div>
         </div>
       )}
       {favProducts.length > 0 && (
         <div
-          className={`${view === "grid"
+          className={`${
+            view === "grid"
               ? "grid grid-cols-4 gap-15 mb-10"
               : "flex flex-col gap-5 mb-10 w-[90%] mx-auto"
-            } my-10 p-10 bg-primary/10 rounded-xl `}
+          } my-10 p-10 bg-primary/10 rounded-xl `}
         >
           {favProducts.map((product) => (
             <FavCard key={product.id} product={product} />
